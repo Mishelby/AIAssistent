f5a51ff16efd6db668b31ec1339f68d72c06fc1c
Misha <lorestatys@Yandex.ru>
2025-08-17 16:53:55 +0300

    add kafka module

---
+++ infrastructure-kafka/src/test/java/ru/development/infrastructurekafka/InfrastructureKafkaApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.development.infrastructurekafka;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled("Disabled")
+class InfrastructureKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
